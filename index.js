004055c5c433e02b27cb7105883d89fa23061fb5
Dektarion <andreiy.boyarsky@gmail.com>
2022-09-05 23:39:10 +0300

    feat: add mobile menu

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-$(function() {
-  $('.scrollup').click(function() {
-    $("html, body").animate({
-    scrollTop:0
-    },300);
-  })
-  })
-  $(window).scroll(function() {
-  if ($(this).scrollTop()>200) {
-    $('.scrollup').fadeIn();
+// $(function() {
+//   $('.scrollup').click(function() {
+//     $("html, body").animate({
+//     scrollTop:0
+//     },300);
+//   })
+//   })
+//   $(window).scroll(function() {
+//   if ($(this).scrollTop()>200) {
+//     $('.scrollup').fadeIn();
+//   }
+//   else {
+//     $('.scrollup').fadeOut();
+//   }
+//   });
+
+
+const mobileMenuIcon = document.querySelector('.header__mobile-hamburger');
+const mobileMenuLine = document.querySelectorAll('.header__mobile-hamburger-line');
+const mobileNavMenu = document.querySelector('.header__nav_mobile');
+
+function mobileMenu() {
+  mobileMenuIcon.classList.add('open');
+  mobileNavMenu.classList.add('open');
+}
+
+function removeOpen() {
+  mobileMenuIcon.classList.remove('open');
+  mobileNavMenu.classList.remove('open');
+}
+
+document.addEventListener ('click', (e) => {
+  if (e.target === mobileMenuIcon || e.target === mobileMenuLine[0] || e.target === mobileMenuLine[1] || e.target === mobileMenuLine[2])  {
+    mobileMenu();
+  } else if (e.target !== e.currentTarget) {
+    removeOpen();
   }
-  else {
-    $('.scrollup').fadeOut();
-  }
-  });
+  return;
+});
